8639189d66af06f60b18e512e399cfe0bf60e03d
charles barros <charlesbarrosdaluz@gmail.com>
2024-06-13 12:01:38 -0300

    curso

---
+++ CursoCucumber/src/test/java/AprenderCucumber.java
@@ -0,0 +1,24 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+public class AprenderCucumber {
+	@Given("que eu criei o arquivo de feature corretamente")
+	public void que_eu_criei_o_arquivo_de_feature_corretamente() {
+	    // Write code here that turns the phrase above into concrete actions
+	//	System.out.println("passou aqui!");
+	   // throw new io.cucumber.java.PendingException();
+	}
+
+	@When("eu o executo")
+	public void eu_o_executo() {
+	    // Write code here that turns the phrase above into concrete actions
+	   // throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("a execução deve ser finalizada com sucesso")
+	public void a_execução_deve_ser_finalizada_com_sucesso() {
+	    // Write code here that turns the phrase above into concrete actions
+	  //  throw new io.cucumber.java.PendingException();
+	}
+
+}
---
+++ CursoCucumber/src/test/java/aprender_cucumber.feature
@@ -0,0 +1,11 @@
+
+Feature: Aprender Cucumber
+	Como um aluno
+	Eu quero aprender a utilizar o Cucumber
+	Para que eu possa automatizar critérios de aceitação
+	
+	Scenario: Deve executar especificação
+  		Given que eu criei o arquivo de feature corretamente
+  		When eu o executo
+  		Then a execução deve ser finalizada com sucesso
+  		
